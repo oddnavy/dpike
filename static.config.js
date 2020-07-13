ae503a38928f8eda4644aad51b88838d316f127b
David Pike <david.pike@inlight.com.au>
2020-07-13 16:52:27 +1000

    adjust preload config for font

--- static.config.js
+++ static.config.js
@@ -1,17 +1,18 @@
-import axios from 'axios'
-import React from 'react'
+import axios from "axios";
+import React from "react";
 
-const INSTAGRAM_BASE_URL = 'https://graph.instagram.com'
+const INSTAGRAM_BASE_URL = "https://graph.instagram.com";
 
 export default {
-  Document: ({
-    Html, Head, Body, children,
-  }) => (
+  Document: ({ Html, Head, Body, children }) => (
     <Html lang="en-GB">
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Personal website of David Pike Frontend Web Developer" />
+        <meta
+          name="description"
+          content="Personal website of David Pike Frontend Web Developer"
+        />
         <meta name="author" content="David Pike" />
         <title>David Pike - Senior Frontend Web Developer</title>
         <link
@@ -19,7 +20,7 @@ export default {
           href="/fonts/source-sans-pro-v11-latin-regular.woff2"
           as="font"
           type="font/woff2"
-          crossOrigin="true"
+          crossOrigin="anonymous"
         />
       </Head>
       <Body>{children}</Body>
@@ -27,33 +28,36 @@ export default {
   ),
 
   getSiteData: () => ({
-    title: 'David Pike',
+    title: "David Pike",
   }),
   getRoutes: async () => {
     return [
       {
-        path: '/',
-        component: 'src/containers/Home',
+        path: "/",
+        component: "src/containers/Home",
       },
       {
-        path: '/posts',
-        component: 'src/containers/Posts',
+        path: "/posts",
+        component: "src/containers/Posts",
         getData: async () => {
-          const request = await axios.get(`${INSTAGRAM_BASE_URL}/17841400291485644/media`, {
-            params: {
-              access_token: process.env.INSTAGRAM_ACCESS_TOKEN,
-              fields: 'caption,media_url,permalink',
-            },
-          });
+          const request = await axios.get(
+            `${INSTAGRAM_BASE_URL}/17841400291485644/media`,
+            {
+              params: {
+                access_token: process.env.INSTAGRAM_ACCESS_TOKEN,
+                fields: "caption,media_url,permalink",
+              },
+            }
+          );
           return { posts: request.data.data };
         },
       },
       {
         is404: true,
-        component: 'src/containers/404',
+        component: "src/containers/404",
       },
-    ]
+    ];
   },
 
-  siteRoot: 'https://www.dpike.co.uk',
-}
+  siteRoot: "https://www.dpike.co.uk",
+};

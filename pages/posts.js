e29afff50e0185428214e9afd3cf77f851dca3d0
David Pike <david.pike@inlight.com.au>
2021-02-14 13:20:34 +1100

    remove unused static config

--- pages/posts.js
+++ pages/posts.js
@@ -1,26 +1,3 @@
-const instagramEndpoint = `https://www.instagram.com/graphql/query/?query_hash=9dcf6e1a98bc7f6e92953d5a61027b98&variables={"id":"361225716","first":16}`;
-
-export async function getStaticProps() {
-  const response = await fetch(instagramEndpoint).then((res) => res.json());
-  const posts = response.data.user.edge_owner_to_timeline_media.edges.map((edge) => {
-    return {
-      id: edge.node.id,
-      media_url: edge.node.display_resources[0].src,
-      width: edge.node.display_resources[0].config_width,
-      height: edge.node.display_resources[0].config_height,
-      thumbnail_url: edge.node.is_video ? $edge.node.thumbnail_resources[2].src : null,
-      caption: edge.node.edge_media_to_caption.edges[0] ? edge.node.edge_media_to_caption.edges[0].node.text : '',
-    };
-  });
-
-  return {
-    props: {
-      posts,
-    },
-    revalidate: 60 * 60 * 24, // 1 day in seconds
-  };
-}
-
 export default function PostsPage({ posts }) {
   return (
     <div className="container container--narrow">
@@ -54,4 +31,27 @@ export default function PostsPage({ posts }) {
       </p>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const instagramEndpoint = `https://www.instagram.com/graphql/query/?query_hash=9dcf6e1a98bc7f6e92953d5a61027b98&variables={"id":"361225716","first":16}`;
+  const response = await fetch(instagramEndpoint).then((res) => res.json());
+  const posts = response.data.user.edge_owner_to_timeline_media.edges.map((edge) => {
+    return {
+      id: edge.node.id,
+      media_url: edge.node.display_resources[0].src,
+      width: edge.node.display_resources[0].config_width,
+      height: edge.node.display_resources[0].config_height,
+      thumbnail_url: edge.node.is_video ? $edge.node.thumbnail_resources[2].src : null,
+      caption: edge.node.edge_media_to_caption.edges[0] ? edge.node.edge_media_to_caption.edges[0].node.text : '',
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 60 * 60 * 24, // 1 day in seconds
+    fallback: 'blocking',
+  };
 }

5d188871cc97ab3fa6fba8ce70fe311d3df4b58c
David Pike <david.pike@inlight.com.au>
2021-05-01 08:59:27 +1000

    use console.error not log

--- pages/posts.js
+++ pages/posts.js
@@ -64,7 +64,7 @@ export async function getStaticProps() {
       };
     });
   } catch (error) {
-    console.log('Error fetching Instagram posts', { error });
+    console.error('Error fetching Instagram posts', { error });
   }
 
   return {

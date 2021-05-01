089483cbf99ac5cf2f5ba2d38ebc611367437d3e
David Pike <david.pike@inlight.com.au>
2021-05-01 11:04:40 +1000

    add some more domains

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   images: {
-    domains: ['scontent-syd2-1.cdninstagram.com', 'content-lax3-1.cdninstagram.com'],
+    domains: [
+      'scontent-syd2-1.cdninstagram.com',
+      'scontent-sjc3-1.cdninstagram.com',
+      'content-lax3-1.cdninstagram.com',
+    ],
   },
 };

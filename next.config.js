3c291f01edc3e6e428c78ee49740f5c730e0d583
David Pike <david.pike@inlight.com.au>
2022-01-23 11:31:36 +1100

    add new instagram cdn domain

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   images: {
     domains: [
-      'scontent-syd2-1.cdninstagram.com',
-      'scontent-sjc3-1.cdninstagram.com',
-      'scontent-sjc3-1.cdninstagram.com',
-      'scontent-lax3-1.cdninstagram.com',
-      'scontent-lax3-2.cdninstagram.com',
-      'scontent-sea1-1.cdninstagram.com',
-      'scontent-lax3-2.cdninstagram.com',
+      "scontent-ort2-2.cdninstagram.com",
+      "scontent-syd2-1.cdninstagram.com",
+      "scontent-sjc3-1.cdninstagram.com",
+      "scontent-sjc3-1.cdninstagram.com",
+      "scontent-lax3-1.cdninstagram.com",
+      "scontent-lax3-2.cdninstagram.com",
+      "scontent-sea1-1.cdninstagram.com",
+      "scontent-lax3-2.cdninstagram.com",
     ],
   },
 };

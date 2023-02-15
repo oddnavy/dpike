a7efad87607ccb43f66eeccab000c461210f9ad4
David Pike <david@dpike.co.uk>
2023-02-16 09:13:56 +1100

    try removing cookie header

--- pages/posts.js
+++ pages/posts.js
@@ -52,11 +52,7 @@ export async function getStaticProps() {
   let posts = [];
 
   try {
-    const response = await fetch(instagramEndpoint, {
-      headers: {
-        cookie: `sessionid=${process.env.INSTAGRAM_COOKIE}`,
-      },
-    }).then((res) => res.json());
+    const response = await fetch(instagramEndpoint).then((res) => res.json());
     posts = response.data.user.edge_owner_to_timeline_media.edges.map(
       (edge) => {
         return {

64c0e7ac2dd60e6a06ab16081eac6c34f1265151
David Pike <david@dpike.co.uk>
2023-02-16 09:11:13 +1100

    switch to remotePattern for images

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 module.exports = {
   images: {
-    domains: [
-      "scontent-ort2-2.cdninstagram.com",
-      "scontent-syd2-1.cdninstagram.com",
-      "scontent-sjc3-1.cdninstagram.com",
-      "scontent-sjc3-1.cdninstagram.com",
-      "scontent-lax3-1.cdninstagram.com",
-      "scontent-lax3-2.cdninstagram.com",
-      "scontent-sea1-1.cdninstagram.com",
-      "scontent-lax3-2.cdninstagram.com",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.fbcdn.net',
+        port: '',
+      },
     ],
   },
 };
